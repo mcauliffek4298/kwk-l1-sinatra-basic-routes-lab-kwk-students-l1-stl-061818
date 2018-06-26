cb4812b641ba3fd28fe2599466440379d4cf85e3
mcauliffek4298 <kathryn@peerlift.org>
2018-06-26 16:38:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
-require_relative 'config/environment'
+require 'sinatra'
 
-class App < Sinatra::Base
+class App < Sinatra :: Base
+  
+  get '/' do 
+    "My name is Kathryn." 
+    
+  end
+  
+    get '/hometown' do
+    "My hometown is St. Louis."
+  end
+  
+  get '/song' do
+    "My favorite-song is Sweatpants, by Childish Gambino"
+end 
+
 end
